bc554215ebac91f6c7db15f66b3ae920b70d496f
clluncor-lang <clluncor@pucp.edu.pe>
2026-05-15 16:20:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("script funcionando");
+alert("mapa funcionando");
+
+var map = L.map('map').setView([-12.068, -77.08], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+L.marker([-12.068, -77.08])
+  .addTo(map)
+  .bindPopup("PUCP")
+  .openPopup();
